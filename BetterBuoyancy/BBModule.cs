eca374dbbf5094460d79e16e6b93ae53a125aa08
ferram4 <ferram40@gmail.com>
2014-12-26 00:13:31 -0500

    Sort of functional, odd behavior when attempting water landings with large horizontal velocities

--- BetterBuoyancy/BBModule.cs
+++ BetterBuoyancy/BBModule.cs
@@ -26,14 +26,15 @@ namespace BetterBuoyancy
                 moduleInitialized = true;
             }
 
-            part.rigidbody.AddForceAtPosition(BuoyancyForce(part.rigidbody), part.transform.position);
+            part.rigidbody.AddForceAtPosition((Vector3)BuoyancyForce(part.rb), part.transform.position);
         }
 
         private Vector3d BuoyancyForce(Rigidbody body)
         {
             //We assume the ocean is at 0 altitude
             double depth = -FlightGlobals.getAltitudeAtPos((Vector3d)part.transform.position, vessel.mainBody);
-            if (depth < 0)
+
+            if (depth < 0)      //corresponds to above the ocean
             {
                 if(part.Splashed)
                 {
@@ -46,14 +47,13 @@ namespace BetterBuoyancy
 
             if(!part.Splashed)
             {
-                ApplySplashEffect();
                 part.WaterContact = true;
                 vessel.Splashed = true;
-                part.rigidbody.drag = 3;
             }
+            ApplySplashEffect();
+
             if (CheckDieOnHighVelocity(body))
                 return Vector3.zero;
-
 
             double vol;
             double depthForMaxForce;
@@ -61,24 +61,28 @@ namespace BetterBuoyancy
             {
                 Vector3 size = part.collider.bounds.size;
                 vol = size.magnitude;       //This is highly approximate, but it works
-                depthForMaxForce = Math.Max(size.x, Math.Max(size.y, size.z));      //This is a very, very rough model of partial immersion
+                depthForMaxForce = Math.Max(Math.Max(size.x, Math.Max(size.y, size.z)), 2);      //This is a very, very rough model of partial immersion
             }
             else
             {
                 vol = 1;
-                depthForMaxForce = 1;
+                depthForMaxForce = 2;
             }
 
-            
-            Vector3d buoyancyForce = -FlightGlobals.getGeeForceAtPosition(part.transform.position) * Math.Min(depth / depthForMaxForce, 1);
-            buoyancyForce *= vol * part.rb.mass;
+            double depthFactor = depth / depthForMaxForce;
+            depthFactor = Math.Min(depthFactor, 1);
+
+            part.rigidbody.drag = 3 * (float)depthFactor;
+
+            Vector3d buoyancyForce = -FlightGlobals.getGeeForceAtPosition(part.transform.position) * depthFactor;
+            buoyancyForce *= vol * OCEAN_DENSITY_IN_TONNE_PER_METER_CUBED;
             return buoyancyForce;
         }
 
         private void ApplySplashEffect()
         {
             float mag = rigidbody.velocity.magnitude * 0.1f;
-            if(mag > 20)
+            if(mag > 5)
                 FXMonger.Splash(part.transform.position, mag);
         }
 
@@ -89,14 +93,14 @@ namespace BetterBuoyancy
             double vertVec = Vector3d.Dot(velVector, vessel.upAxis);
             if (vertVec > part.crashTolerance * VERT_CRASH_TOL_FACTOR)
             {
-                GameEvents.onCrashSplashdown.Fire(new EventReport(FlightEvents.SPLASHDOWN_CRASH, part, part.partInfo.title, "blah", 0, ""));
+                GameEvents.onCrashSplashdown.Fire(new EventReport(FlightEvents.SPLASHDOWN_CRASH, part, part.partInfo.title, "", 0, ""));
                 part.Die();
                 return true;
             }
             double horizVel = (velVector - vertVec * vessel.upAxis).magnitude;
             if (horizVel > part.crashTolerance * HORIZ_CRASH_TOL_FACTOR)
             {
-                GameEvents.onCrashSplashdown.Fire(new EventReport(FlightEvents.SPLASHDOWN_CRASH, part, part.partInfo.title, "blah", 0, ""));
+                GameEvents.onCrashSplashdown.Fire(new EventReport(FlightEvents.SPLASHDOWN_CRASH, part, part.partInfo.title, "", 0, ""));
                 part.Die();
                 return true;
             }
